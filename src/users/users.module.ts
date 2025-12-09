d265076dfdde580b0f01f602f131a8123cfd16ea
d <d>
2025-12-10 01:55:59 +0200

    fix: Register all entities in UsersModule to fix TypeORM error

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
-
 import { Video } from '../videos/entities/video.entity';
+import { ParentalConsent } from '../parental-consent/parental-consent.entity';
+import { BlockedUser } from './entities/blocked-user.entity';
+import { UserInterest } from './entities/user-interest.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Video])],
+  imports: [TypeOrmModule.forFeature([User, Video, ParentalConsent, BlockedUser, UserInterest])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
+
